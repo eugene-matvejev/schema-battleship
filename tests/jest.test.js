b0e62cfa7f311e3f48df6f16199486c61508476c
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-08-18 17:04:54 +0100

    improved tests

--- tests/jest.test.js
+++ tests/jest.test.js
@@ -1,41 +1,61 @@
 const Validator = require('jsonschema').Validator;
 const v = new Validator();
 
-const schemaDir = `${__dirname}/../schema/`;
-const validMockDir = `${__dirname}/../mock-valid/`;
-const inValidMockDir = `${__dirname}/../mock-invalid/`;
+const schemaDir      = `${__dirname}/../schema/`;
+const mockDir        = `${__dirname}/../mock`;
+const validMockDir   = `${mockDir}/valid/`;
+const inValidMockDir = `${mockDir}/invalid/`;
 
 const fs = require('fs');
 
-const schemaFiles = fs.readdirSync(schemaDir);
-const validMockFiles = fs.readdirSync(validMockDir);
-const inValidMockFiles = fs.readdirSync(inValidMockDir);
+const schemaFiles      = fs.readdirSync(schemaDir);
+const validMockFiles   = fs.readdirSync(validMockDir).map(filename => { return {filename: filename, sequence: 0x00} });
+const inValidMockFiles = fs.readdirSync(inValidMockDir).map(filename => { return {filename: filename, sequence: 0x00} });
 
-const readJSONFile = (path) => {
+const parseJSON = (path) => {
     return JSON.parse(fs.readFileSync(path, 'utf8'));
 };
-const assertMock = (absolutePath, fileName, schema, routeAlias, criteria,) => {
-    const regex = new RegExp(`^${routeAlias}`);
+const assertMock = (absolutePath, fileName, schema, result) => {
+    it(`${fileName}`, () => {
+        const json = parseJSON(`${absolutePath}`);
 
-    if (regex.test(fileName)) {
-        it(`${fileName}`, () => {
-            const json = readJSONFile(`${absolutePath}`);
-
-            expect(v.validate(json, schema).valid).toBe(criteria);
-        });
-    }
+        expect(v.validate(json, schema).valid).toBe(result);
+    });
 };
+const dataProvider = schemaFiles.map(filename => {
+    const regex = new RegExp(`^${filename.replace(`.schema.json`, ``)}`);
+
+    return {
+        schemaFile: filename,
+        mocks: {
+            invalid: inValidMockFiles.map(filename => {
+                if (regex.test(filename.filename)) {
+                    filename.sequence |= 0x01;
+
+                    return filename.filename;
+                }
+            }),
+            valid: validMockFiles.map(filename => {
+                if (regex.test(filename.filename)) {
+                    filename.sequence |= 0x01;
+
+                    return filename.filename;
+                }
+            })
+        }
+    }
+});
+
 describe(`validate mocks against schemas`, () => {
-    schemaFiles.forEach(schemaFile => {
-        describe(`:: ${schemaFile}`, () => {
-            const routeAlias = schemaFile.replace(`.schema.json`, ``);
-            const schema = readJSONFile(`${schemaDir}/${schemaFile}`);
+    dataProvider.forEach(el => {
+        describe(`:: ${el.schemaFile}`, () => {
+            const schema = parseJSON(`${schemaDir}/${el.schemaFile}`);
 
             describe(`:: should be valid`, () => {
-                validMockFiles.forEach(file => assertMock(`${validMockDir}/${file}`, file, schema, routeAlias, true));
+                el.mocks.valid.forEach(file => assertMock(`${validMockDir}/${file}`, file, schema, true))
             });
             describe(`:: should be invalid`, () => {
-                inValidMockFiles.forEach(file => assertMock(`${inValidMockDir}/${file}`, file, schema, routeAlias, false));
+                el.mocks.invalid.forEach(file => assertMock(`${inValidMockDir}/${file}`, file, schema, false));
             });
         })
     });
