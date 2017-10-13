4e8c67d1cd496eba50718aae9b16e0215aaf439c
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-10-13 10:00:52 +0100

    Update jest.test.js

--- tests/jest.test.js
+++ tests/jest.test.js
@@ -2,15 +2,15 @@ const Validator = require('jsonschema').Validator;
 const v         = new Validator();
 
 const schemaDir      = `${__dirname}/../schema/`;
-const mockDir        = `${__dirname}/../mock`;
-const validMockDir   = `${mockDir}/valid/`;
-const inValidMockDir = `${mockDir}/invalid/`;
+const stubDir        = `${__dirname}/../stubs`;
+const validMockDir   = `${stubDir}/valid/`;
+const inValidMockDir = `${stubDir}/invalid/`;
 
 const fs = require('fs');
 
 const schemaFiles      = fs.readdirSync(schemaDir);
-const validMockFiles   = fs.readdirSync(validMockDir).map(el => ({ name: el, sequence: 0x00 }));
-const inValidMockFiles = fs.readdirSync(inValidMockDir).map(el => ({ name: el, sequence: 0x00 }));
+const validStubFiles   = fs.readdirSync(validMockDir).map(el => ({ name: el, sequence: 0x00 }));
+const inValidStubFiles = fs.readdirSync(inValidMockDir).map(el => ({ name: el, sequence: 0x00 }));
 
 const parseJSON = (path) => {
     return JSON.parse(fs.readFileSync(path, 'utf8'));
@@ -22,11 +22,11 @@ const assertMock = (absolutePath, fileName, schema, result) => {
         expect(v.validate(json, schema).valid).toBe(result);
     });
 };
-const dataProvider = schemaFiles.map(filename => {
-    const regex = new RegExp(`^${filename.replace(`.schema.json`, ``)}`);
+const dataProvider = schemaFiles.map((f) => {
+    const regex = new RegExp(`^${f.replace(`.schema.json`, ``)}`);
 
     const invalid = [];
-    inValidMockFiles.forEach(el => {
+    inValidStubFiles.forEach((el) => {
         if (regex.test(el.name)) {
             el.sequence |= 0x01;
 
@@ -34,7 +34,7 @@ const dataProvider = schemaFiles.map(filename => {
         }
     });
     const valid = [];
-    validMockFiles.forEach(el => {
+    validStubFiles.forEach((el) => {
         if (regex.test(el.name)) {
             el.sequence |= 0x01;
 
@@ -43,39 +43,39 @@ const dataProvider = schemaFiles.map(filename => {
     });
 
     return {
-        schemaFile: filename,
-        mocks: {
+        schemaFile: f,
+        stubs: {
             invalid: invalid,
             valid: valid
         }
     }
 });
 
-describe(`validate mocks against schemas`, () => {
-    dataProvider.forEach(el => {
+describe(`validate stubs against schemas`, () => {
+    dataProvider.forEach((el) => {
         describe(`${el.schemaFile}`, () => {
             const schema = parseJSON(`${schemaDir}/${el.schemaFile}`);
 
             describe(`:: should be valid`, () => {
-                el.mocks.valid.forEach(el => assertMock(`${validMockDir}/${el.name}`, el.name, schema, true))
+                el.stubs.valid.forEach(el => assertMock(`${validStubDir}/${el.name}`, el.name, schema, true))
             });
             describe(`:: should be invalid`, () => {
-                el.mocks.invalid.forEach(el => assertMock(`${inValidMockDir}/${el.name}`, el.name, schema, false));
+                el.stubs.invalid.forEach(el => assertMock(`${inValidStubDir}/${el.name}`, el.name, schema, false));
             });
         })
     });
 });
 
-describe(`every mock should have a schema`, () => {
-    validMockFiles.forEach((file) => {
-        it(`${file.name}`, () => {
-            expect(file.sequence).toBeGreaterThan(0)
+describe(`every stub should have a schema`, () => {
+    validMockFiles.forEach((f) => {
+        it(`${f.name}`, () => {
+            expect(f.sequence).toBeGreaterThan(0)
         });
     });
 
-    inValidMockFiles.forEach((file) => {
-        it(`${file.name}`, () => {
-            expect(file.sequence).toBeGreaterThan(0)
+    inValidMockFiles.forEach((f) => {
+        it(`${f.name}`, () => {
+            expect(f.sequence).toBeGreaterThan(0)
         });
     });
 });
